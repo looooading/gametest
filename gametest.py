80833f39b2bf66de4da1078ceb43f397ec65a34c
looooading <54234042+looooading@users.noreply.github.com>
2020-02-25 17:02:38 +0800

    Add files via upload

---
+++ gametest.py
@@ -0,0 +1,12 @@
+print("有信号了，太好了。")
+print("hello hello 能听见吗")
+temp = input("1.是的 我能听见  2.你是谁？（回复1或2，进行选择）")
+answer = int(temp)
+if answer == 1:
+    print("太好了，终于成功了。我是莉娅。我已经在这该死的机器面前趴了40多分钟了，真不敢相信他说的是真的。")
+    print("这很复杂，说来话长。在这之前，你能先告诉我你叫什么吗？")
+else:
+    print("我叫莉娅。你叫什么？")
+temp = input()
+name = int(temp)
+print("name ，嗯，好名字")
